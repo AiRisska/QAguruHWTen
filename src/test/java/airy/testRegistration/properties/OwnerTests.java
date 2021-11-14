9cc6d3d551bd8f033dc41a37ffdbfba8f5977b9d
AiRisska <74139561+A@users.noreply.github.com>
2021-11-14 21:50:38 +0300

    Tag in OwnerTests

--- src/test/java/airy/testRegistration/properties/OwnerTests.java
+++ src/test/java/airy/testRegistration/properties/OwnerTests.java
@@ -2,10 +2,12 @@ package airy.testRegistration.properties;
 
 import airy.config.CredentialsConfig;
 import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static java.lang.String.format;
 
+@Tag("properties")
 public class OwnerTests {
     public CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
     @Test
